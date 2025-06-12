cfb06560171bbcfe659b4c4ee208f2ea086e2545
Hannah <hannahbgoldstein@gmail.com>
2025-06-12 17:59:29 -0300

    Front home, conectar views

--- FavoritosBR/baseFilmes/urls.py
+++ FavoritosBR/baseFilmes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.filmes, name="filmes"),
+    path("", views.filmes_lista, name="filmes"),
+    path("roleta/", views.filmes_roleta, name="filmes_roleta"),
 ]
--- FavoritosBR/baseFilmes/views.py
+++ FavoritosBR/baseFilmes/views.py
@@ -4,6 +4,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-def filmes(request):
-    return render(request,"filmes.html")
+# def filmes(request):
+#     return render(request,"filmes.html")
 
+def filmes_lista(request):
+    genero = request.GET.get("genero")
+    filmes = [...]  # seu código de filmes aqui
+    if genero:
+        filmes = [f for f in filmes if f["genero"] == genero]
+    return render(request, "filmes_lista.html", {"filmes": filmes, "genero": genero})
+
+
+def filmes_roleta(request):
+    return render(request, "filmes_roleta.html")
